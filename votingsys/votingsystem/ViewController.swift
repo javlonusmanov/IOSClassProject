dfd06c5c66d9b2f766cb53d03d45c8070af71f09
kostaqpapa <kp967@nyu.edu>
2014-11-18 10:27:17 -0800

    Initial Commit

---
+++ votingsys/votingsystem/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  votingsystem
+//
+//  Created by Kosta on 11/14/14.
+//  Copyright (c) 2014 KostaqPapa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
